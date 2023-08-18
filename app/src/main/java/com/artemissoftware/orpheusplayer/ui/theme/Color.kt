edbb34779e236800886405ca8dfdab51dd891aa4
gustavo <gustavo.isaac@gmail.com>
2023-08-18 05:22:47 +0100

    Colors and themes

--- app/src/main/java/com/artemissoftware/orpheusplayer/ui/theme/Color.kt
+++ app/src/main/java/com/artemissoftware/orpheusplayer/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Blue200 = Color(0xFF30189F)
+val Blue500 = Color(0xFF483974)
+val Blue700 = Color(0xFF1c1348)
+val Orange200 = Color(0xFFeb7603)
